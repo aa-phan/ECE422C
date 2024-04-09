e4a26fe776f1c8691f5a3629cf730ffcb26f6255
Aaron Phan <atp2323@utexas.edu>
2024-04-09 01:07:00 -0500

    started on test cases

--- assignment6/CinemaTest.java
+++ assignment6/CinemaTest.java
@@ -8,4 +8,29 @@ public class CinemaTest {
 
     // Write your JUnit tests here
 
+    //test that a cinema is properly generated and functions normally
+    @Test
+    public void testA(){
+
+    }
+    //case where a cinema with no spots is created
+    @Test
+    public void testB(){
+
+    }
+    //super large case with multiple customers, ensure functionality under high stress
+    @Test
+    public void testC(){
+
+    }
+    //case where not enough spots exist
+    @Test
+    public void testD(){
+
+    }
+    //case where every customer is downgraded to a lower seat
+    @Test
+    public void testE(){
+
+    }
 }
